a5cb927f44eb6b858ba5f38502097d2ee7b9ff12
joao <jovmamikl@gmail.com>
2026-04-24 07:38:47 -0300

    Initialize dataAnalytics project with Maven setup and basic application structure

---
+++ src/test/java/com/dataAnalytics/dataAnalytics/DataAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataAnalytics.dataAnalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
